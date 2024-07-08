a57ce2225594a9e1c4d53b1146732c871c8d2265
Sergio Flores <sergio3004.2002@gmail.com>
2024-07-08 17:15:50 -0500

    first commit

---
+++ HH_Object_Detection/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,6 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+    const val MODEL_PATH = "model_test_v3.tflite"
+    const val LABELS_PATH = "labels_v3.txt"
+}
---
+++ PPE_Object_Detection/android_app/app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,6 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+    const val MODEL_PATH = "model_test_v2.tflite"
+    const val LABELS_PATH = "labels.txt"
+}
